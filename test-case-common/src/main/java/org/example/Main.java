8c68f0efe25aff8425f9b5ed6ff6d903ba937780
yawkat <jonas.konrad@oracle.com>
2023-02-03 11:08:33 +0100

    remove logging from main

--- test-case-common/src/main/java/org/example/Main.java
+++ test-case-common/src/main/java/org/example/Main.java
@@ -1,22 +1,9 @@
 package org.example;
 
-import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.Micronaut;
-import io.micronaut.runtime.event.annotation.EventListener;
-import jakarta.inject.Singleton;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-@Singleton
 public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         Micronaut.run(args);
-    }
-
-    @EventListener
-    protected void onStartup(StartupEvent event) {
-        LOGGER.info("Running on Java {}", System.getProperty("java.version"));
     }
 }
